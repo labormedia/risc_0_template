4b36a572368280c58bb2044d998e161b5197e266
Diego Correa T <algoritmia@labormedia.cl>
2023-04-25 22:55:36 +0000

    first commit

---
+++ methods/guest/src/main.rs
@@ -0,0 +1,13 @@
+#![no_main]
+// If you want to try std support, also update the guest Cargo.toml file
+#![no_std]  // std support is experimental
+
+
+// TODO: Uncomment the line below for tools for communicating with the host
+// use risc0_zkvm::guest::env;
+
+risc0_zkvm::guest::entry!(main);
+
+pub fn main() {
+    // TODO: Implement your guest code here
+}
